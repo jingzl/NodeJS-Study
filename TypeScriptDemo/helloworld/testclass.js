4a451e0c38b8f162556c04d9905ea96687b023ac
jingzl <jingzl@hotmail.com>
2025-06-06 07:19:37 +0800

    add testclass.ts

---
+++ TypeScriptDemo/helloworld/testclass.js
@@ -0,0 +1,12 @@
+var Person = /** @class */ (function () {
+    function Person(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    Person.prototype.greet = function () {
+        console.log("Hello, my name is ".concat(this.name, " and I am ").concat(this.age, " years old."));
+    };
+    return Person;
+}());
+var alice = new Person('Alice', 30);
+alice.greet();
